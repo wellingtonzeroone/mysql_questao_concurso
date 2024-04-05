e5f5e6297485ce4e0e38c4d146eabf8d8eb2498a
wellingtonzeroone <naovalido3@gmail.com>
2024-04-05 20:47:06 -0300

    05.04

---
+++ tabelas.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Prato(
+	codPrato INT NOT NULL auto_increment,
+    tipo VARCHAR(10) NOT NULL,
+    nome VARCHAR(30) NOT NULL,
+    PRIMARY KEY (codPrato)
+);
+
+CREATE TABLE Bebida(
+	codBeb INT NOT NULL auto_increment,
+    nome VARCHAR(20) NOT NULL,
+    descrBeb VARCHAR(50) NOT NULL,
+    PRIMARY KEY(codBeb)
+);
+
+CREATE TABLE CARDAPIO(
+	codCli INT NOT NULL,
+    nome VARCHAR(40) NOT NULL,
+    codBeb INT,
+    PRIMARY KEY (codCli),
+    FOREIGN KEY (codBeb) REFERENCES Bebida(codBeb)
+);
+
+CREATE TABLE T1(
+	codPrt INT NOT NULL,
+    codCli INT NOT NULL,
+    PRIMARY KEY(codPrt, codCli),
+    FOREIGN KEY(codPrt) REFERENCES Cardapio(codPrato),
+    FOREIGN KEY(codCli) REFERENCES Cardapio(codCli)
+);
+
+CREATE TABLE Cliente (
+  codCli INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(40) NOT NULL,
+  PRIMARY KEY (codCli)
+);
